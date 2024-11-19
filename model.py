2cee49065caa3cd2181d89a8fde7e8188e75ce2a
Barb <barbara.oluoch@strathmore.edu>
2024-11-19 09:14:29 +0300

    Create the customer model

---
+++ model.py
@@ -0,0 +1,11 @@
+from django.db import models
+# Create your models here.
+class Customer(models.Model):
+    # It represents a customer in the e-commerce system with fields: 
+    # name: the name of the customer
+    # email: the email of the customer(should be unique to each customer) 
+    # phone number: the phone number of the customer
+    # address: the address of the customer
+    # created_at: the date and time when the customer was created
+
+    
