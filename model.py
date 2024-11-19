d90d20479d1a26f96d34d16d437634505396eaed
Barb <barbara.oluoch@strathmore.edu>
2024-11-19 09:20:10 +0300

    customer address field in the customer model

--- model.py
+++ model.py
@@ -11,4 +11,5 @@ class Customer(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20)
+    address = models.TextField()
    
